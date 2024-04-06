09e1dca800ed368e7a75bb946524e7691d46dfd4
minsoo <mmkim1210@gmail.com>
2024-04-05 17:43:52 -0700

    more

--- src/HS.jl
+++ src/HS.jl
@@ -39,11 +39,28 @@ begin
     end
     df.lesion_loc_combined = storage
     df.lesion_loc_n = length.(storage)
+    df.adi_natrank[df.adi_natrank .== "."] .= missing
+    df.adi_natrank = Vector{Union{Missing, Int}}(df.adi_natrank)
+    df.adi_staterank[df.adi_staterank .== "."] .= missing
+    df.adi_staterank = Vector{Union{Missing, Int}}(df.adi_staterank)
+    df.svi_socio_econ[df.svi_socio_econ .== "."] .= missing
+    df.svi_socio_econ = Vector{Union{Missing, Float64}}(df.svi_socio_econ)
+    df.svi_hcomp[df.svi_hcomp .== "."] .= missing
+    df.svi_hcomp = Vector{Union{Missing, Float64}}(df.svi_hcomp)
+    df.svi_mino_lang[df.svi_mino_lang .== "."] .= missing
+    df.svi_mino_lang = Vector{Union{Missing, Float64}}(df.svi_mino_lang)
+    df.svi_htype_trans[df.svi_htype_trans .== "."] .= missing
+    df.svi_htype_trans = Vector{Union{Missing, Float64}}(df.svi_htype_trans)
+    df.svi_total[df.svi_total .== "."] .= missing
+    df.svi_total = Vector{Union{Missing, Float64}}(df.svi_total)
 end
 
 begin
     df.er_visits_total_ucla .= Int.(df.er_visits_total_ucla)
 end
 
-unique(df.adi_natrank)
-freqtable(df, "adi_natrank")
+show(unique(df.svi_total))
+freqtable(df, "income")
+
+# ignored the following covariates: zip, race, marital_status, sexual_orientation, religion, occupation, dx_duration, hurley_stage, primary_ruci, sec_ruci
+# ignored the following outcome variables: er_visits_total_all and management/treatment-related outcomes
